96cbe2b45018d458f7f984a8b387280fd1ded11d
KKH(15P-XD) <ke4145@gmail.com>
2023-10-29 21:15:32 +0900

    상태 머신 구현 및 이벤트 핸들 수정
    - 플레이어 이동 테스트
    - game_input 모듈 내부 함수를 game_engine으로 이동

--- game_engine.py
+++ game_engine.py
@@ -1,5 +1,4 @@
 from pico2d import *
-import game_input
 import game_world
 from player import Player
 from constant_value import SCREEN_W, SCREEN_H
@@ -17,14 +16,27 @@ def init():
 
 def run():
     while game_enable:
-        game_input.handle_events()
+        handle_events()
         update_world()
         render_world()
         delay(0.01)
 
     close_canvas()
         
+
+def handle_events():
+    global game_enable
+
+    events = get_events()
+    for event in events:
+        if event.type == SDL_QUIT:
+            game_enable = False
+        elif event.type == SDL_KEYDOWN and event.key == SDLK_ESCAPE:
+            game_enable = False
+        else:
+            player.handle_event(event)
         
+
 def init_world():
     global player
 
--- player.py
+++ player.py
@@ -1,24 +1,28 @@
 from pico2d import load_image
 from constant_value import SCREEN_W, SCREEN_H
+from state_machine import StateMachine
 
 
 class Player:
+    image = None
+
     def __init__(self):
-        global image
-        
-        image = None
         self.x, self.y = SCREEN_W / 2, SCREEN_H // 2
         self.frame = 0
         self.action = 3
-        if image == None:
-            image = load_image('res\\player.png')
+        self.dir = 0
+        self.face_dir = 1
+        self.state_machine = StateMachine(self)
+        self.state_machine.start()
+        if Player.image == None:
+            Player.image = load_image('res\\player.png')
 
     def update(self):
-        pass
+        self.state_machine.update()
 
     def handle_event(self, event):
-        pass
+        self.state_machine.handle_event(('INPUT', event))
 
     def draw(self):
-        image.clip_draw(self.frame * 100, self.action * 100, 100, 100, self.x, self.y)
+        self.state_machine.draw()
     
